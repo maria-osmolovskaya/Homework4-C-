04c7eb17ea592d649d84697d9a08b58dbf8a7a90
maria-osmolovskaya <119137585+maria-osmolovskaya@users.noreply.github.com>
2022-12-15 01:15:38 +0300

    Add files via upload

---
+++ Program04-01.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+
+*/
+
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else 
+        {
+            Console.WriteLine("Ввели не число. Введите корректное число");
+        }
+    }
+    return result;
+}
+
+int NumberA = GetNumber ("Введите первое число");
+int NumberB = GetNumber ("Введите второе число");
+
+Console.WriteLine ($"{Math.Pow(NumberA,NumberB)}");
